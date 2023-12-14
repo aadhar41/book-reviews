c8fa3ecb8065b49682612438c404e4711dfff4a6
Aadhar gaur <aadhar41@gmail.com>
2023-12-14 21:43:59 +0530

    013 Popular or Highest Rated - the View

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -5,9 +5,29 @@
 
     <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
         <input type="text" name="title" placeholder="Search by title" value="{{ request('title') }}" class="input h-10" />
+        <input type="hidden" name="filter" value="{{ request('filter') }}" />
         <button type="submit" class="btn h-10">Search</button>
         <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
     </form>
+
+    <div class="filter-container mb-4 flex">
+        @php
+            $filters = [
+                '' => 'Latest',
+                'popular_last_month' => 'Popular Last Month',
+                'popular_last_6month' => 'Popular Last 6 Month',
+                'highest_rated_last_month' => 'Highest Rated Last Month',
+                'highest_rated_last_6month' => 'Highest Rated Last 6 Month',
+            ];
+        @endphp
+
+        @foreach ($filters as $key => $label)
+            <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}"
+                class="{{ request('filter') === $key || (request('filter') == null && $key === '') ? 'filter-item-active' : 'filter-item' }} ">
+                {{ $label }}
+            </a>
+        @endforeach
+    </div>
 
     <ul>
         @forelse ($books as $book)
