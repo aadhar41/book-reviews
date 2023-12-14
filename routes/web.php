54d7d2bdc4c44f8106575d1d68d11e3f61dbefec
Aadhar gaur <aadhar41@gmail.com>
2023-12-14 18:09:30 +0530

    010 Controllers & Resource Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    dd();
 });
+
+
+Route::resource('books', BookController::class);
